7eb28e7a1d1328574fbe89d8d532ce7f37b4f390
mustaphabaimik <mostaphabaimik@gmail.com>
2020-10-20 15:43:42 +0200

    firstcommit

---
+++ app.js
@@ -0,0 +1,87 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+
+
+const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+  allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
+
+/*app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+  app.options('*', (req, res) => {
+    // allowed XHR methods
+    res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+    res.send();
+  });
+});*/
+
+const produitsRouter = require('./routes/produits');
+const commandeRouter = require('./routes/commande');
+
+app.use("/api/produits",produitsRouter);
+app.use("/api/commandes",commandeRouter);
+
+
+
+/*app.use(function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept ,  Authorization");
+  if(req.method ==='OPTIONS'){
+    res.header('Access-Control-Allow-Methods','GET', 'PUT', 'DELETE', 'PATCH', 'POST');
+    return res.status(200).json({});
+  }
+
+});*/
+
+/*app.use(cors({
+  origin: '*',
+  methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+  allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));*/
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/produits.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const router = express.Router();
+const {database}=require("../config/db");
+
+
+/* method getall() */
+router.get('/', function (req, res) {
+    let page = (req.query.page !== undefined && req.query.page !== 0) ? req.query.page : 1;
+    const limit = (req.query.limit !== undefined && req.query.limit !== 0) ? req.query.limit : 10;
+    let startValue;
+    let endValue;
+    if (page > 0) {
+        startValue = (page * limit) - limit;     // 0, 10, 20, 30
+        endValue = page * limit;                  // 10, 20, 30, 40
+    } else {
+        startValue = 0;
+        endValue = 10;
+    }
+    database.table('products as p')
+        .join([
+            {
+                table: "categories as c",
+                on: `c.id = p.cat_id`
+            }
+        ])
+        .withFields(['c.title as category',
+            'p.title as name',
+            'p.price',
+            'p.quantity',
+            'p.description',
+            'p.image',
+            'p.id'
+        ])
+        .slice(startValue, endValue)
+        .sort({id: .1})
+        .getAll()
+        .then(prods => {
+            if (prods.length > 0) {
+                res.status(200).json({
+                    nbr: prods.length,
+                    produits: prods
+                });
+            } else {
+                res.json({message: "Aucun produits trouvé"});
+            }
+        })
+        .catch(err => console.log(err));
+});
+
+//method get one
+router.get('/:idProduit', (req, res) => {
+    let idProd = req.params.idProduit;
+
+    database.table('products as p')
+        .join([
+            {
+                table: "categories as c",
+                on: `c.id = p.cat_id`
+            }
+        ])
+        .withFields(['c.title as category',
+            'p.title as name',
+            'p.price',
+            'p.quantity',
+            'p.description',
+            'p.image',
+            'p.id',
+            'p.images'
+        ])
+        .filter({'p.id': idProd})
+        .get()
+        .then(prod => {
+            console.log(prod);
+            if (prod) {
+                res.status(200).json(prod);
+            } else {
+                res.json({message: `Aucun produit ne correspond à ce produit. ${productId}`});
+            }
+        }).catch(err => res.json(err));
+});
+
+// produit par categorie
+router.get('/categorie/:cattitre', (req, res) => {
+    let page = (req.query.page !== undefined && req.query.page !== 0) ? req.query.page : 1;
+    const limit = (req.query.limit !== undefined && req.query.limit !== 0) ? req.query.limit : 10;
+    let startValue;
+    let endValue;
+    if (page > 0) {
+        startValue = (page * limit) - limit;
+        endValue = page * limit;
+    } else {
+        startValue = 0;
+        endValue = 10;
+    }
+
+
+    const titre_cat = req.params.cattitre;
+
+    database.table('products as p')
+        .join([
+            {
+                table: "categories as c",
+                on: `c.id = p.cat_id WHERE c.title LIKE '%${titre_cat}%'`
+            }
+        ])
+        .withFields(['c.title as category',
+            'p.title as name',
+            'p.price',
+            'p.quantity',
+            'p.description',
+            'p.image',
+            'p.id'
+        ])
+        .slice(startValue, endValue)
+        .sort({id: 1})
+        .getAll()
+        .then(prods => {
+            if (prods.length > 0) {
+                res.status(200).json({
+                    count: prods.length,
+                    products: prods
+                });
+            } else {
+                res.json({message: `Aucun produit trouvé dans cette categorié ${cat_title}`});
+            }
+        }).catch(err => res.json(err));
+
+});
+
+
+module.exports = router;
